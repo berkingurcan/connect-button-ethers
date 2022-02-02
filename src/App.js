adc4972355e35fb5320df971cf8113708334b862
berkingurcan <berkingurcan@yandex.com>
2022-02-02 20:28:46 +0300

    sda

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import './App.css';
-import { ethers, BigNumber } from "ethers"
-import { useEffect, useState, useRef } from 'react'
-import Connect from './components/Connect';
+import "./App.css";
+import { ethers, BigNumber } from "ethers";
+import { useEffect, useState, useRef } from "react";
+import Connect from "./components/Connect";
 
 function App() {
-  
-
   return (
     <div className="App">
       <Connect />
--- src/components/Connect.js
+++ src/components/Connect.js
@@ -1,50 +1,55 @@
-import React from 'react'
-import { ethers, BigNumber } from "ethers"
-import { useEffect, useState, useRef } from 'react'
-import Button from '@mui/material/Button'
-import ButtonGroup from '@mui/material/ButtonGroup';
-import TextField from '@mui/material/TextField';
-import Typography from '@mui/material/Typography';
-import swal from 'sweetalert';
+import React from "react";
+import { ethers, BigNumber } from "ethers";
+import { useEffect, useState, useRef } from "react";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import swal from "sweetalert";
 
 const Connect = () => {
-    const [accounts, setAccounts] = useState([])
-    const [btnAddress, setBtnAddress] = useState("Connect")
-    const [isConnected, setIsConnected] = useState(false)
-    const [accBalance, setAccBalance] = useState()
+  const [accounts, setAccounts] = useState([]);
+  const [btnAddress, setBtnAddress] = useState("Connect");
+  const [isConnected, setIsConnected] = useState(false);
+  const [accBalance, setAccBalance] = useState();
 
-    async function connectToMetamask(){
-        if (window.ethereum) {
-            let provider;
-            try {
-                provider = new ethers.providers.Web3Provider(window.ethereum, 4002);
-            } catch (err) {
-                console.log('HATA' + err)
-                swal('PLEASE SWITCH FANTOM TESTNET')
-            }
-            if(provider){
-                await provider.send("eth_requestAccounts", []);
-            }
-            const signer = provider.getSigner();
-            const addressOfAccount = await signer.getAddress()
-            const balanceOfAddress = await provider.getBalance(addressOfAccount)
-            const balanceOfAddressLast = parseInt((balanceOfAddress._hex).toString(16),16)/10**18
-            setIsConnected(true)
-            setBtnAddress(addressOfAccount)
-        
-            console.log(parseInt((balanceOfAddress._hex).toString(16),16)/10**18)
-            setAccBalance(balanceOfAddressLast)
-            } else {
-            swal('Please install Metamask')
-            }
+  async function connectToMetamask() {
+    if (window.ethereum) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+      if ((await provider.getNetwork()).chainId !== 4002) {
+        swal("PLEASE SWITCH FANTOM TESTNET");
+        return;
+      }
+
+      await provider.send("eth_requestAccounts", []);
+      const signer = provider.getSigner();
+      const addressOfAccount = await signer.getAddress();
+      const balanceOfAddress = await provider.getBalance(addressOfAccount);
+      const balanceOfAddressLast =
+        parseInt(balanceOfAddress._hex.toString(16), 16) / 10 ** 18;
+      setIsConnected(true);
+      setBtnAddress(addressOfAccount);
+
+      console.log(parseInt(balanceOfAddress._hex.toString(16), 16) / 10 ** 18);
+      setAccBalance(balanceOfAddressLast);
+    } else {
+      swal("Please install Metamask");
     }
+  }
   return (
     <div>
-    <Button onClick={connectToMetamask} variant="contained">
-        {isConnected ? <Typography>{btnAddress.slice(0,8) + "...  "}[{accBalance} FTM]</Typography> : "Connect"}
-    </Button>
+      <Button onClick={connectToMetamask} variant="contained">
+        {isConnected ? (
+          <Typography>
+            {btnAddress.slice(0, 8) + "...  "}[{accBalance} FTM]
+          </Typography>
+        ) : (
+          "Connect"
+        )}
+      </Button>
     </div>
-  )
-}
+  );
+};
 
-export default Connect
+export default Connect;
